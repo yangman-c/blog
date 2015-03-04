ec7fd01b4bc297129150510b4b7922980164c200
mayyang <ym0913@126.com>
2015-03-04 17:54:46 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -48,9 +48,11 @@ module Blog
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
 
     I18n.enforce_available_locales = false
 
